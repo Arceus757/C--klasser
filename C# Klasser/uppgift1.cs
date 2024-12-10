d64776822da3bd4b7f7495a0d29560c2a2162320
Dmytro <dmytro.malanchuk@elev.ga.ntig.se>
2024-12-10 13:28:22 +0100

    first commit

---
+++ C# Klasser/uppgift1.cs
@@ -0,0 +1,38 @@
+﻿
+
+public class Circle
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetCircumference()
+    {
+        return 2 * Math.PI * radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+
+    public double Radius
+    {
+        get { return radius; }
+        set { radius = value; }
+    }
+ }
+
+ class Program
+ {
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5.0);
+        Console.WriteLine("Radius: " + circle.Radius);
+        Console.WriteLine("Circumference: " + circle.GetCircumference());
+        Console.WriteLine("Area: " + circle.GetArea());
+    }
+}
